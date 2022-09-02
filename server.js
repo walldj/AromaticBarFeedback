86a7bb24838a5500c013331ffe7ef97ab354dab2
wallabhjoshi <wallabhjoshi@gmail.com>
2022-09-02 20:11:15 +0530

    added folders and added to server.js 1. proper file structure 2. basic server capabilities

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
- 
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const app = express();
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+
+//log of requests
+app.use(morgan('tiny'))
+
+app.get('/',(req,res)=>{
+    res.send("CRUD");
+})
+
+app.listen(PORT, ()=>{console.log(`Server running on http://localhost:${PORT}`)})
